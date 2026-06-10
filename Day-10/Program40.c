c6456a2a404a394388ce14113a5c4f0b757103ff
Aayushk75 <hyperblimph690@gmail.com>
2026-06-10 11:44:08 +0530

    Day-10 uploaded.

---
+++ Day-10/Program40.c
@@ -0,0 +1,21 @@
+// Program to print character pyramid
+#include <stdio.h>
+int main() {
+    int rows = 5;
+
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= rows - i; j++) {
+            printf(" ");
+        }
+        char ch = 'A';
+        for (int k = 1; k <= i; k++) {
+            printf("%c", ch++);
+        }
+        ch -= 2;
+        for (int l = i - 1; l >= 1; l--) {
+            printf("%c", ch--);
+        }
+        printf("\n");
+    }
+    return 0;
+}
